3916e95c1b990c669864b3f5e03e8cc0bf764fc0
ShunanZhao <oas1sgh@bosch.com>
2024-04-25 13:16:27 +0800

    this is a test

--- 2test.py
+++ 2test.py
@@ -1,1 +1,14 @@
-print(":)")
+import pandas as pd
+
+# Specify the correct path to your Excel file
+excel_file = r'C:\Users\oas1sgh\Desktop\max\Copy of Max ZHAO_Onboarding checklist_230721.xlsx'
+
+# Read the Excel file into a pandas DataFrame
+df = pd.read_excel(excel_file)
+
+# Print the contents of the DataFrame
+print(df)
+
+# Check for missing values in df
+missing_values = df.isnull().sum()
+print(missing_values)
